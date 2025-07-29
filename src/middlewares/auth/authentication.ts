fdecd5d8a82e40a2e7b232a96dc229092a27740e
Dinh Canh <dinhcanhh2004@gmail.com>
2025-07-29 17:17:24 +0700

    feat: add authenticate middleware

---
+++ src/middlewares/auth/authentication.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import ApiError from "~/utils/ApiError.js";
+import { verifyToken } from "~/utils/signToken.js";
+
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token || !token.startsWith("Bearer")) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, "Authorization token missing");
+    }
+
+    const access_token = token.split(" ")[1];
+
+    const decoded_access_token = await verifyToken({ token: access_token });
+    req.user = decoded_access_token;
+    next();
+  } catch (error: any) {
+    next(new ApiError(StatusCodes.UNAUTHORIZED, error.message));
+  }
+};
