e27f3b685e2871151ca2b8f16d98e2bbe98a8925
Dinh Canh <dinhcanhh2004@gmail.com>
2025-08-12 20:37:39 +0700

    feat: add public statis file

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,19 @@ import rootRouter from "~/routes/index.js";
 import dotenv from "dotenv";
 import { errorHandler } from "./middlewares/error.js";
 import { initFolderUploads } from "~/utils/fileUpload.js";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8888;
 // create folder uploads
 initFolderUploads();
+
+// static file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/images", express.static(path.join(__dirname, "../uploads")));
 
 //convert body to JSON
 app.use(express.json());
