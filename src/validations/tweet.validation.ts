3435d6925509fcf5bc120fd5089b55ee69459095
Dinh Canh <dinhcanhh2004@gmail.com>
2025-09-19 23:18:39 +0700

    fix: validation for tweet

--- src/validations/tweet.validation.ts
+++ src/validations/tweet.validation.ts
@@ -3,8 +3,8 @@ import Joi from "joi";
 export const createTweet = Joi.object({
   type: Joi.number().integer().valid(0, 1, 2, 3).required(),
   audience: Joi.number().integer().valid(0, 1, 2, 3).required(),
-  content: Joi.string().required(),
-  parent_id: Joi.number().allow(null),
+  content: Joi.string().allow(""),
+  parent_id: Joi.string().allow(""),
   hashtags: Joi.array().items(Joi.string()).required(),
   mentions: Joi.array().items(Joi.string()).required(),
   medias: Joi.array().items(Joi.string()).required()
